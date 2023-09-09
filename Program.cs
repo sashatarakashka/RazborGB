f7e374e76f4e5ea92f5276d29b9d07d56355f00b
sashkatarakashka <lapakoza@gmail.com>
2023-09-09 23:51:10 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите число : ");
+string text = Console.ReadLine();
+int x = Convert.ToInt32(text);
+switch(x)
+{
+    case 1 :
+    Console.WriteLine("Понедельник");
+    break;
+    case 2 :
+    Console.WriteLine("Вторник");
+    break;
+    case 3 :
+    Console.WriteLine("Среда");
+    break;
+    case 4 :
+    Console.WriteLine("Четверг");
+    break;
+    case 5 :
+    Console.WriteLine("Пятница");
+    break;
+    case 6 :
+    Console.WriteLine("Суббота");
+    break;
+    case 7 :
+    Console.WriteLine("Воскресенье");
+    break;
+    default :
+    Console.WriteLine("Нет такого дня недели, введите число от 1 до 7");
+    break;
+}
+Console.WriteLine("The End");
